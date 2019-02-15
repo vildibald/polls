86dfd235c3b9d2fdfd5e03f60c4f41f74aed6fb1
viliam <viliam.kacala.ml@gmail.com>
2019-02-15 22:31:31 +0100

    Fixed HSQLDB merge.

--- src/test/resources/data-hsqldb.sql
+++ src/test/resources/data-hsqldb.sql
@@ -1,21 +1,4 @@
--- SET DATABASE SQL SYNTAX PGS TRUE;
-
-
--- INSERT INTO roles(id, name)
--- VALUES (1, 'ROLE_USER'),
---        (2, 'ROLE_ADMIN')
--- ON CONFLICT DO NOTHING;
-
--- MERGE INTO roles USING(
--- VALUES (1, 'ROLE_USER'),
---        (2, 'ROLE_ADMIN')
--- ON roles.id =
--- ) WHEN NOT MATCHED THEN INSERT VALUES
---   (1, 'ROLE_USER'),
---   (2, 'ROLE_ADMIN');
-
 MERGE INTO roles USING(VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN'))
-  AS vals(x, name) ON roles.id = vals.id
-  WHEN NOT MATCHED THEN INSERT VALUES vals.id, vals.name
+  AS vals(id, name) ON roles.id = vals.id
+  WHEN NOT MATCHED THEN INSERT VALUES vals.id, vals.name;
 --   WHEN MATCHED THEN UPDATE SET r.name = vals.name
-
