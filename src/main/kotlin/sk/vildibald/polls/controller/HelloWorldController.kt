4590dcc7f595940367e8bd5a1f954c8e976cd800
viliam <viliam.kacala.ml@gmail.com>
2019-02-18 20:21:52 +0100

    Added log settings

--- src/main/kotlin/sk/vildibald/polls/controller/HelloWorldController.kt
+++ src/main/kotlin/sk/vildibald/polls/controller/HelloWorldController.kt
@@ -1,13 +1,8 @@
 package sk.vildibald.polls.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
-
 /**
  * Dummy controller.
  */
 interface HelloWorldController {
-    @GetMapping("/hello")
-    @ResponseBody
     fun hello(): String
 }
